19963d8d72e1247735643608302fe95c3e51a30e
joelmukanya <joel@lifechoices.co.za>
2022-06-05 14:06:40 +0200

    Example on \r

---
+++ code.js
@@ -0,0 +1,9 @@
+/*
+\n: New line
+\r: This will set the cursor to point to the start of the current line.
+*/
+let data = 'Hello There\nMy name is Joel\rand am a software developer';
+console.log(data);
+/*
+Based on the above example, we see that "I am a software developer" is written on top of "My name is Joel". 
+*/
